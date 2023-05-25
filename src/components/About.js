4936afa797a1ba285180e937dfb86dbf89130403
Natasa288 <natasajovic238@gmail.com>
2023-05-25 10:07:47 +0200

    ab

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,14 @@
+import "./About.css";
+
 const About = () =>{
     return <div><h1>About Page</h1>
     </div>
 }
 export default About;
+
+<div>
+<p>This is the about page of the BlaBla Chat Application. </p>
+    <p> With our app, you can easily share messages with your friends and stay connected.</p>
+<p>Our mission is to provide a seamless and enjoyable messaging experience. Whether you're chatting one-on-one or in a group, BlaBla Chat has got you covered!</p>
+<p>Start conversations and stay connected with the people who matter most to you. Join BlaBla Chat today!</p>
+</div>
